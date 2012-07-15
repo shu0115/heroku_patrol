b69d66fd2ce9a9b3a12cafb0d3d19190ffdc43fa
shu0115 <raisondetre0115@gmail.com>
2012-07-16 02:56:06 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPatrol::Application.config.secret_token = '378088404d57d5a2acb6661663e74cd7884dfd93c8f4f4d40152369fec7dcdee02e5d74b409a4510cd2213ac7d46d71a29e61cd5084412f2a7f9f022a333c9d7'
